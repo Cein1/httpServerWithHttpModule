9781bdec8917ea53cf218aa01f8b30b649adddd0
heeyeon <k.heeyeon22@gmail.com>
2022-09-30 22:28:20 +0900

    [ADD] 게시물 등록(생성) 엔드포인트

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ const posts = [
   {
     id: 1,
     title: "간단한 HTTP API 개발 시작!",
-    description: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
     userId: 1,
   },
   {
     id: 2,
     title: "HTTP의 특성",
-    description: "Request/Response와 Stateless!!",
+    content: "Request/Response와 Stateless!!",
     userId: 1,
   },
 ];
@@ -56,11 +56,28 @@ const httpRequestListener = function (request, response) {
               password : user.password
             });
 
-            response.writeHead(200, {'Content-Type' : 'application/json'});
+            response.writeHead(201, {'Content-Type' : 'application/json'});
             response.end(JSON.stringify({"message" : "userCreated"}));
 
         });
-    }; 
+    } else if (url === '/posts') {
+        let body = '';
+        request.on('data', (data) => {body += data});
+
+        request.on('end', () => {
+          const post = JSON.parse(body);
+
+          posts.push({
+            id: post.id,
+            title: post.title,
+            content: post.content,
+            userId: post.userId
+          });
+
+          response.writeHead(201, {'Content-Type' : 'application/json'});
+          response.end(JSON.stringify({"message" : "postCreated"}));
+      });
+    }
   }
 };
 
