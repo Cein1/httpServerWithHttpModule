33aa2151de3da7d59ef49d2ef8e13d6d5108d536
heeyeon <k.heeyeon22@gmail.com>
2022-10-03 19:16:21 +0900

    [ADD] 유저 회원가입 엔드포인트 구현

--- app.js
+++ app.js
@@ -33,35 +33,37 @@ const posts = [
 
 const httpRequestListener = function (request, response) {
   const { url, method } = request;
+  
   if (method === 'GET') {
     if (url === '/ping') {
       response.writeHead(200, {'Content-Type' : 'application/json'});
       response.end(JSON.stringify({message : 'pong'}));
-    }
-  }
+    };
+  };
 
   if (method === 'POST') { // (3)
-    if (url === '/users/signup') {
-        let body = ''; // (4)
-        request.on('data', (data) => {body += data}); // (5)
-        
-        // stream을 전부 받아온 이후에 실행
-        request.on('end', () => {  // (6)
-            const user = JSON.parse(body); //(7) 
+    if (url === '/users/signup') { //과제 1번
+      let body = ''; // (4)
+      request.on('data', (data) => {body += data}); // (5)
+      
+      // stream을 전부 받아온 이후에 실행
+      request.on('end', () => {  // (6)
+        const user = JSON.parse(body); //(7) 
 
-            users.push({ // (8)
-              id : user.id,
-              name : user.name,
-              email: user.email,
-              password : user.password
-            });
-
-            response.writeHead(201, {'Content-Type' : 'application/json'});
-            response.end(JSON.stringify({"message" : "userCreated"}));
-
+        users.push({ // (8)
+          id : user.id,
+          name : user.name,
+          email: user.email,
+          password : user.password
         });
-    } 
-  }
+
+        response.writeHead(201, {'Content-Type' : 'application/json'});
+        response.end(JSON.stringify({"message" : "userCreated"}));
+
+      });
+    };
+  };
+
 };
 
 server.on("request", httpRequestListener);
@@ -69,5 +71,4 @@ server.on("request", httpRequestListener);
 server.listen(8000, '127.0.0.1', function() { 
     console.log('Listening to requests on port 8000');
 });
-// 서버를 껐다 켜게 되면, users 변수는 메모리에 값을 저장하기 때문에 저장했던 모든 값이 다시 초기화가 됩니다. 그래서 데이터를 영구적으로 저장할 수 있는 데이터베이스 시스템이 필요합니다.
-
+/* 서버를 껐다 켜게 되면, users 변수는 메모리에 값을 저장하기 때문에 저장했던 모든 값이 다시 초기화가 됩니다. 그래서 데이터를 영구적으로 저장할 수 있는 데이터베이스 시스템이 필요합니다.*/
